43a975d539d2edc6129889c6e241e3cacf705e29
omarnabulsi16 <31221375+omarnabulsi16@users.noreply.github.com>
2019-02-28 15:26:54 -0800

    added main menu

---
+++ Knock Off Game/Assets/Scenes/quit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class quit : MonoBehaviour
+{
+    public void doesquit()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+    }
+}
+
