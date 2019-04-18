410fc3f34c903c92871f98e092b8dc93a506d9d9
omarnabulsi16 <31221375+omarnabulsi16@users.noreply.github.com>
2019-04-18 16:32:21 -0700

    next level screen

--- Knock Off Game/Assets/levelcomplete.cs
+++ Knock Off Game/Assets/levelcomplete.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class levelcomplete : MonoBehaviour
 {
+    // Update is called once per frame
     public void LoadNextLevel ()
     {
+        //score.text = "Your Score: " + scoreValue;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        ScoreScript.scoreValue = 0;
     }
 }
