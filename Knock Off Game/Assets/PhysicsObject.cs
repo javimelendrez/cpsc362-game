016cc7daa2ee65a625a16ef1fcffb3625fd0d963
Javier Melendrez <44476129+javimelendrez@users.noreply.github.com>
2019-04-24 15:05:30 -0700

    trying to merge

--- Knock Off Game/Assets/PhysicsObject.cs
+++ Knock Off Game/Assets/PhysicsObject.cs
@@ -121,7 +121,7 @@ public class PhysicsObject : MonoBehaviour
         if (rb2d.position.x > 232.37)
         {
 
-            FindObjectOfType<GameManage>().EndGame();
+            FindObjectOfType<GameManage>().CompleteLevel();
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
