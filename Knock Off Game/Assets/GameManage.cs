1e76e9344e59819f8ad9190772a78d0cb5dfffe7
Javier Melendrez <44476129+javimelendrez@users.noreply.github.com>
2019-04-24 15:02:44 -0700

    trying to merge

--- Knock Off Game/Assets/GameManage.cs
+++ Knock Off Game/Assets/GameManage.cs
@@ -5,15 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class GameManage : MonoBehaviour
 {
-    bool gameHasEnded = false;
-    public float restartDelay = 1f;
-    public GameObject completLevelUI;
-
-    public void CompleteLevel()
-    {
-        completLevelUI.SetActive(true);
-    }
-
+    /*
+     *     bool gameHasEnded = false;
     public void EndGame()
     {
         if (gameHasEnded == false)
@@ -26,6 +19,46 @@ public class GameManage : MonoBehaviour
     void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        ScoreScript.scoreValue = 0;
+    }
+     */
+    public static bool GameIsOver = false;
+    public GameObject gameOverMenuUI;
+    public float restartDelay = 1f;
+    public GameObject completLevelUI;
+
+    public void CompleteLevel()
+    {
+        completLevelUI.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameIsOver)
+        {
+
+            EndGame();
+        }
+    }
+    public void EndGame()
+    {
+        gameOverMenuUI.SetActive(true);
+        //SoundManager.StopMusic();
+        Time.timeScale = 0f;
+        GameIsOver = true;
+       
+
+    }
+    public void RestartButton()
+    {
+        Restart();
+    }
+    void Restart()
+    {
+        gameOverMenuUI.SetActive(false);
+        GameIsOver = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
         ScoreScript.scoreValue = 0;
     }
 }
