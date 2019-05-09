416b8caa249f35992255f09ea3f79aab4082157f
IWearAThai <tkluu@csu.fullerton.edu>
2019-05-09 05:58:21 -0700

    Added vertical enemies

---
+++ Knock Off Game/Assets/enemyVertical.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyVertical : MonoBehaviour
+{
+	public float freq;
+	public float amp;
+	Vector3 posOffset = new Vector3();
+	Vector3 tempPos = new Vector3();
+	void Start()
+	{
+		posOffset = transform.position;
+	}
+	void Update()
+	{
+		tempPos = posOffset;
+
+		transform.position = new Vector3(transform.position.x, tempPos.y + Mathf.Sin(Time.fixedTime * Mathf.PI * freq) * amp, transform.position.z);
+	}
+}
+
+
